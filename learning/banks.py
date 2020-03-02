a70006ea4deceef85c3fe0689c6d5368e0cdbc2d
az-ahmad <aziz.ahmad.contact@gmail.com>
2020-03-02 20:19:18 +0000

    update

--- learning/banks.py
+++ learning/banks.py
@@ -1,3 +1,4 @@
+_users = {}
 
 class Bank:
     def __init__(self,bankname):
@@ -5,6 +6,12 @@ class Bank:
         self.bankname = bankname
     def __str__(self):
         print(f'You are at at {self.bankname} bank')
+
+    def getUsers(self):
+        for a,b in _users.items():
+            if b[0] == self.bankname:
+                print(f'User : {a}, Branch: {b[1]}')
+
 
 class Branch(Bank):
     def __init__(self,bankname,branchname):
@@ -14,13 +21,20 @@ class Branch(Bank):
     
     def __str__(self):
         print(f'You are at at {self.branchname} bank')
+    
+    def getUsers(self):
+        for a,b in _users.items():
+            if b[0] == self.bankname and b[1]== self.branchname:
+                print(a)
 
 class Person(Branch,Bank):
-    def __init__(self,name='John Smith',money=100,bankname='Halifax',branchname='Hounslow'):
+    global users
+    def __init__(self,name='John Smith',money=100,bankname='Halifax',branchname='Kensington'):
         Branch.__init__(self,bankname,branchname)
         self.name = name
         self.money = money
-    
+        _users[self.name] = [self.bankname,self.branchname]
+
     def __str__(self):
         return(f'Your name is {self.name}, you have £{self.money} at the {self.branchname} {self.bankname}')
 
@@ -33,13 +47,22 @@ class Person(Branch,Bank):
         else:
             print('Insufficient funds')
 
-person1 = Person('Aziz',300,'Barclays','Feltham')
+    def getUsers(self):
+        print('Does nothing')
+
+person1 = Person('Mr Red',300,'Barclays','Mayfair')
+person2 = Person('Mr Blue',600,'Barclays','Mayfair')
+person3 = Person('Mr Green',115,'Halifax','Bricky')
+person4 = Person('Mr Purple',60,'Halifax','Mayfair')
+person5 = Person('Mr Gold',10,'Metro','Egham')
 
 print(person1.name)
 print(person1.money)
 person1.deposit(25)
 print(person1.money)
 person1.withdraw(600)
-print(person1.branchname)
-print(person1.bankname)
+
 print(person1)
+
+bank1 = Bank('Barclays')
+bank1.getUsers()
