e259271e462764ad92b0eb3cce853bf20ad7000d
az-ahmad <aziz.ahmad.contact@gmail.com>
2020-03-04 00:38:46 +0000

    update

--- learning/banks.py
+++ learning/banks.py
@@ -1,25 +1,25 @@
-_users = {}
+import fileinput
 
 class Bank:
     def __init__(self,bankname):
-        print(f'Bank: {bankname} created')
         self.bankname = bankname
     def __str__(self):
         print(f'You are at at {self.bankname} bank')
         print('To list the users and their respective branches, call getUsers() on this object')
 
     def getUsers(self):
-        try:
-            for a,b in _users.items():
-                if b[0] == self.bankname:
-                    print(f'User : {a}, Branch: {b[1]}')
-        except:
-            print('Something went wrong!')
+        with open('bankdata.txt', 'r') as file:
+            for line in file:
+                if line.split(',')[2] == self.bankname:
+                    print('Name:', line.split(',')[0], ', Money:', line.split(',')[1], ', Branch:', line.rstrip().split(',')[3])
+        file.close()
+        branchQuery = input('\nIf you would like to query a specific branch, please enter the branch\'s name: ')
+        branch = Branch(self.bankname,branchQuery)
+        branch.getUsers()
 
 
 class Branch(Bank):
     def __init__(self,bankname,branchname):
-        print(f'{branchname} branch created')
         Bank.__init__(self,bankname)
         self.branchname = branchname
     
@@ -28,12 +28,10 @@ class Branch(Bank):
         print('To list the users of this branch, call getUsers() on this object')
     
     def getUsers(self):
-        try:
-            for a,b in _users.items():
-                if b[0] == self.bankname and b[1]== self.branchname:
-                    print(a)
-        except:
-            print('Something went wrong!')
+        with open('bankdata.txt', 'r') as file:
+            for line in file:
+                if line.rstrip().split(',')[3] == self.branchname and line.split(',')[2] == self.bankname:
+                    print('Name:', line.split(',')[0], ', Money:', line.split(',')[1])
 
 class Person(Branch,Bank):
     global users
@@ -41,7 +39,11 @@ class Person(Branch,Bank):
         Branch.__init__(self,bankname,branchname)
         self.name = name
         self.money = money
-        _users[self.name] = [self.bankname,self.branchname]
+        userDict = {self.name:[self.bankname,self.branchname]}
+        with open('bankdata.txt', 'a+') as file:
+            file.write(f'{self.name},{self.money},{self.bankname},{self.branchname}\n')
+        
+        # _users[self.name] = [self.bankname,self.branchname]
 
     def __str__(self):
         return(f'Your name is {self.name}, you have £{self.money} at the {self.branchname} {self.bankname}')
@@ -61,22 +63,62 @@ class Person(Branch,Bank):
     def getUsers(self):
         raise NotImplementedError("Function can only be called on Bank or Branch instances")
 
-person1 = Person('Mr Red',300,'Barclays','Mayfair')
-person2 = Person('Mr Blue',600,'Barclays','Mayfair')
-person3 = Person('Mr Green',115,'Halifax','Bricky')
-person4 = Person('Mr Purple',60,'Halifax','Mayfair')
-person5 = Person('Mr Gold',10,'Metro','Egham')
+def main():
+    try:
+        print('\nHello and welcome to Online Banking\n')
+        print('Please type \'0\' to check your account')
+        print('Please type \'1\' to create a new account')
+        print('Please type \'2\' to query a bank\'s data')
+        bankingScreen = int(input('Enter your selection: '))
+        if bankingScreen == 1:
+            newCustomer()
+        elif bankingScreen == 2:
+            bankQuery = input('\nWhich bank would you like to query? ')
+            bank = Bank(bankQuery)
+            bank.getUsers()
+        elif bankingScreen ==0:
+            withdrawNameQuery = input('\nPlease enter your name as it appears in your bank account: ')
+            with open('bankdata.txt', 'r') as file:
+                for line in file:
+                    if line.split(',')[0] == withdrawNameQuery:
+                        print('Name:', line.split(',')[0], ', Money:', line.split(',')[1], ' Bank:', line.rstrip().split(',')[2] ,', Branch:', line.rstrip().split(',')[3])
+            print('\nPlease type \'1\' to withdraw money NOT IMPLEMENTED')
+            print('Please type \'2\' to deposit money NOT IMPLEMENTED')
+            screenSelection = int(input('Enter your selection: '))
+            if screenSelection==1:
+                withdrawAmount = int(input('How much would you like to withdraw? '))
+                with open('bankdata.txt', 'r+') as file:
+                    for line in file:
+                        if line.split(',')[0] == withdrawNameQuery and int(line.split(',')[1])>=withdrawAmount:
+                            newAmount = int(line.split(',')[1]) - withdrawAmount
+                            print(f'Your new balance is {newAmount}')
+                            newline = line.replace(line.split(',')[1], str(newAmount))
+                            file.write(f'{newline}\n')
+                            print(newline)
+                            '''
 
-print(person1.name)
-print(person1.money)
-person1.deposit(25)
-print(person1.money)
-person1.withdraw(600)
 
-print(person1)
 
-bank1 = Bank('Barclays')
-bank1.getUsers()
-print('')
-branch1 = Branch('Halifax','Bricky')
-branch1.getUsers()
+    except ValueError:
+        raise ValueError
+
+def newCustomer():
+    try:
+        print('\nPlease enter the following details to create your brand new Online Banking account')
+        newName = input('Please enter your full name: ')
+        newMoney = int(input('How much money will you put in your bank account? '))
+        newBank = input('What company is your bank? ')
+        newBranch = input('Where is your local bank branch located? ')
+    except ValueError:
+        raise ValueError
+    person1 = Person(newName,newMoney,newBank,newBranch)
+    # bank1 = Bank('Barclays')
+    # bank1.getUsers()
+
+
+
+
+if __name__=='__main__':
+    main()
+
+
